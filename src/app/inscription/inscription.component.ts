e820e54d82bfaed47ade8c47dec3ce1b56bca376
aissatou7 <yayefallsaliou@gmail.com>
2022-12-07 10:10:15 +0000

    controle de saisie

---
+++ src/app/inscription/inscription.component.ts
@@ -0,0 +1,72 @@
+import { Component, OnInit } from '@angular/core';
+import {FormGroup,FormBuilder,Validators,FormControl} from '@angular/forms';
+@Component({
+  selector: 'app-inscription',
+  templateUrl: './inscription.component.html',
+  styleUrls: ['./inscription.component.css']
+})
+
+export class InscriptionComponent implements OnInit {
+  registerForm !: FormGroup;
+  password = 'password';
+  submitted = false;
+
+  constructor(private formBuilder : FormBuilder) { }
+
+  ngOnInit() {
+      this.registerForm = this.formBuilder.group({
+          firstName: ['', Validators.required],
+          lastName: ['', Validators.required],
+          tel: ['', Validators.required],
+          email: ['', [Validators.required, Validators.email,Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]],
+          password: ['', [Validators.required]],
+          passwordC: ['', [Validators.required, matchValidator(this.password)]],
+          profil: ['', [Validators.required]]
+          
+      });
+  }
+
+
+  
+}
+
+
+export function matchValidator(fieldName: string) {
+  let fcfirst: FormControl;
+  let fcSecond: FormControl;
+
+  return function matchValidator(control: FormControl) {
+
+      if (!control.parent) {
+          return null;
+      }
+
+      // INITIALIZING THE VALIDATOR.
+      if (!fcfirst) {
+          //INITIALIZING FormControl first
+          fcfirst = control;
+          fcSecond = control.parent.get(fieldName) as FormControl;
+
+          //FormControl Second
+          if (!fcSecond) {
+              throw new Error('matchValidator(): Second control is not found in the parent group!');
+          }
+
+          fcSecond.valueChanges.subscribe(() => {
+              fcfirst.updateValueAndValidity();
+          });
+      }
+
+      if (!fcSecond) {
+          return null;
+      }
+
+      if (fcSecond.value !== fcfirst.value) {
+          return {
+              matchOther: true
+          };
+      }
+
+      return null;
+  }
+}
---
+++ src/app/restaurer/restaurer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-restaurer',
+  templateUrl: './restaurer.component.html',
+  styleUrls: ['./restaurer.component.css']
+})
+export class RestaurerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
