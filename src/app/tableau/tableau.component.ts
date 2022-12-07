4208dec6a328a52aa08848992a50fb19e6ad231f
issa <i.ndiaye@isepdiamniadio.edu.sn>
2022-12-07 17:30:51 +0000

    first commit

--- src/app/tableau/tableau.component.ts
+++ src/app/tableau/tableau.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CrudService } from '../services/inscription.service';
 
 @Component({
   selector: 'app-tableau',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TableauComponent implements OnInit {
 
-  constructor() { }
+  Books: any = [];
+
+  constructor(private crudService: CrudService) {}
 
   ngOnInit(): void {
+    this.crudService.GetBooks().subscribe((res) => {
+      console.log(res);
+      this.Books = res;
+    });
+  }
+
+  delete(id: any, i: any) {
+    console.log(id);
+    if (window.confirm('Do you want to go ahead?')) {
+      this.crudService.deleteBook(id).subscribe((res) => {
+        this.Books.splice(i, 1);
+      });
+    }
   }
 
 }
