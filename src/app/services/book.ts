f7b43d24eb80558ddb06bff66afcd59e53c69073
ODIA221 <senbusinessfk@gmail.com>
2022-12-13 10:25:36 +0100

    maj projet

--- src/app/services/book.ts
+++
@@ -1,8 +0,0 @@
-export class Book {
-    _id!: String;
-    firstName!: String;
-    lastName!: String;
-    email!: String;
-    tel!:number;
-    profil!:string;
-  }
--- src/app/shared/user.ts
+++
@@ -1,5 +0,0 @@
-export class User {
-    _id!: String;
-    email!: String;
-    password!: String;
-  }
