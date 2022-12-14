dd1fd4e6fa1fb6960c4cdc8cd3d053e0a08869c9
fadal ba <fadalba@gmail.com>
2022-12-14 13:15:31 +0000

    test

---
+++ src/app/services/book.ts
@@ -0,0 +1,10 @@
+export class Book {
+    _id!: String;
+    firstName!: String;
+    lastName!: String;
+    email!: String;
+    tel!:number;
+    profil!:string;
+    dateinscrit!: Date;
+    matricule!:String;
+  }
